427f39f78b094d6a056861357f968a170cc7d44c
P Gautam <92343715+PGautam27@users.noreply.github.com>
2021-11-26 22:41:10 +0530

    Update Recipe.java

--- Recipe.java
+++ Recipe.java
@@ -1,7 +1,5 @@
-import java.util.HashMap;
 import java.util.*;
-public class Recipe {
-
+public class Recipes {
     static Scanner sc = new Scanner(System.in);
 
     public HashMap<String,ArrayList<String>> addRecipe(){
@@ -27,8 +25,36 @@ public class Recipe {
         return recipes;
     }
 
+    public HashMap<String,ArrayList<String>> addInstruction(HashMap<String,ArrayList<String>> inst){
+        System.out.print("For which recipe do you wanna add the instructions: ");
+        String k = sc.nextLine();
+        for(HashMap.Entry<String,ArrayList<String>> entry : inst.entrySet()){
+            boolean areEqual = k.equals(entry.getKey());
+            if(areEqual){
+                System.out.print("Enter how many instructions you wanna add: ");
+                int r = sc.nextInt();
+                sc.nextLine();
+                for(int i=0;i<r;i++){
+                    System.out.println("Enter the "+(i+1)+" instruction");
+                    String y = sc.nextLine();
+                    inst.get(k).add(y);
+                }
+            }
+        }
+        return inst;
+    }
+
     public void print(HashMap<String,ArrayList<String>> recipes){
-        System.out.print(recipes);
+        for(HashMap.Entry<String,ArrayList<String>> entry : recipes.entrySet()){
+            int i =1;
+            System.out.println("The name of the recipe is: "+entry.getKey());
+            System.out.println("The ingredients and the instructions are as follows: ");
+            for(String inst: entry.getValue()){
+                System.out.println(i+". "+inst);
+                i++;
+            }
+            System.out.println();
+        }
     }
 
     public void printRecipe(HashMap<String,ArrayList<String>> recipes){
@@ -42,7 +68,18 @@ public class Recipe {
         System.out.println("Enter the recipe name: ");  
         String displayName = sc.nextLine();
         if(recipes.get(displayName)!=null){
-            System.out.println(recipes.get(displayName));
+            for(HashMap.Entry<String, ArrayList<String>> entry : recipes.entrySet()){
+                String key = entry.getKey();
+                boolean areEqual = displayName.equals(key);
+                if(areEqual){
+                    int i = 1;
+                    System.out.println("The ingredients and the instructions for the "+displayName+" recipe is: ");
+                    for(String name : entry.getValue()){
+                        System.out.println(i+". "+name);
+                        i++;
+                    }
+                }
+            }
         }
         else{
             System.out.println("Sorry the recipe not found");
