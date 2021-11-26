000707f8f4a9c186ced8ab6160e964fdb22551b0
P Gautam <92343715+PGautam27@users.noreply.github.com>
2021-11-26 22:40:00 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.HashMap;
 class Main{
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
@@ -8,29 +7,34 @@ class Main{
         boolean flag = true;
         
         while(flag){
-            Recipe obj = new Recipe();
+            Recipes obj = new Recipes();
+            System.out.println();
             System.out.println ("Welcome to the Recipe Manager.");
             System.out.println ("Please type the number of the operation you would like to perform:");
-            System.out.println ("1. Add a New Recipe");
-            System.out.println ("2. List All Recipe present");
-            System.out.println ("3. Display Recipe");
-            System.out.println ("4. Search for Recipes by Ingredient");
-            System.out.println ("5. Close the Recipe Application");
+            System.out.println ("1. Add a New Recipe and it's ingredients");
+            System.out.println ("2. Add instruction for the Recipe");
+            System.out.println ("3. List All Recipe present");
+            System.out.println ("4. Display Recipe");
+            System.out.println ("5. Search for Recipes by it's name");
+            System.out.println ("6. Close the Recipe Application");
             int k = sc.nextInt();
             switch(k){
                 case 1:
                     dish = obj.addRecipe();
                     break;
                 case 2:
+                    obj.addInstruction(dish);
+                    break;    
+                case 3:
                    obj.printRecipe(dish);
                     break;
-                case 3:
+                case 4:
                     obj.print(dish);
                     break;
-                case 4:
+                case 5:
                     obj.displayRecipe(dish);
                     break;
-                case 5:
+                case 6:
                     flag = false;
                     break;
                 default:
